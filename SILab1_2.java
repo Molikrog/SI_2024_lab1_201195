1335242ab64fcd0d069d12cf53a6247286ead19a
Martin Simonoski <simonoskimartin68@gmail.com>
2024-04-13 06:34:43 +0200

    Fix wrong condition in makeBigStringFrom

--- SILab1_2.java
+++ SILab1_2.java
@@ -5,8 +5,9 @@ public class SILab1_2 {
     private static String makeBigStringFrom (List<String> list, int minLength) {
         StringBuilder sb = new StringBuilder();
         for (String el : list) {
-            if (el.length() < minLength) {
-                sb.append(el);
+            for (int i = 0; i < el.length(); i += minLength) {
+                int endIndex = Math.min(i + minLength, el.length());
+                sb.append(el.substring(i, endIndex));
             }
         }
         return sb.toString();
