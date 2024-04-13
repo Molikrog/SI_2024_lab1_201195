e6683b61fe8767659071c72ec0dd8ea8b90a2aae
Martin Simonoski <simonoskimartin68@gmail.com>
2024-04-13 19:14:30 +0200

    some blank spaces fix

--- SILab1Main.java
+++ SILab1Main.java
@@ -5,13 +5,10 @@ import java.util.Scanner;
 public class SILab1Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        int n = sc.nextInt();
-
-        List<String> list = new ArrayList<>();
-
-        for (int i = 0; i < n; i++) {
-            list.add(sc.next());
+	int n = sc.nextInt();
+	List<String> list = new ArrayList<>();
+	for (int i = 0; i < n; i++) {
+        list.add(sc.next());
         }
 	System.out.println(SILab1_2.makeBigStringFrom(list));
     }
